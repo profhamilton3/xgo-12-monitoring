0ad7af90162c5f233205d606c85a1970d124567e
T. Hamilton <profhamilton3@gmail.com>
2023-05-10 21:21:47 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 radio.onReceivedValue(function (name, value) {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 200, 1, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.InBackground)
     num = value
     led.plotBarGraph(
-    Math.map(num, 0, 500, 0, 5),
+    Math.map(num, 0, 300, 0, 5),
     5
     )
 })
 let num = 0
 radio.setGroup(99)
-basic.showIcon(IconNames.Giraffe)
+basic.showIcon(IconNames.Snake)
